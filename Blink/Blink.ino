a5ceb4d7ce65f871c912fb908612365a63448268
Reese Johnson <rtfmjohnson@gmail.com>
2016-05-30 16:57:04 -0400

    blink and fade

--- Blink/Blink.ino
+++ Blink/Blink.ino
@@ -11,19 +11,19 @@
 
   modified 8 May 2014
   by Scott Fitzgerald
+  and trexor 5/30/16
  */
 
 
 // the setup function runs once when you press reset or power the board
 void setup() {
   // initialize digital pin 13 as an output.
-  pinMode(13, OUTPUT);
 }
 
 // the loop function runs over and over again forever
 void loop() {
-  digitalWrite(13, HIGH);   // turn the LED on (HIGH is the voltage level)
+  digitalWrite(5, HIGH);   // turn the LED on (HIGH is the voltage level)
   delay(1000);              // wait for a second
-  digitalWrite(13, LOW);    // turn the LED off by making the voltage LOW
+  digitalWrite(5, LOW);    // turn the LED off by making the voltage LOW
   delay(1000);              // wait for a second
 }
