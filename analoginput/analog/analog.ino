79759d5d1c1a815c0a7dc42cd7bd7e85465c1cc5
Reese Johnson <rtfmjohnson@gmail.com>
2016-06-26 20:49:44 -0400

    photo cell and potentiometer

--- analoginput/analog/analog.ino
+++ analoginput/analog/analog.ino
@@ -35,15 +35,8 @@ void setup() {
 }
 
 void loop() {
-  // read the value from the sensor:
-  sensorValue = analogRead(sensorPin);    
-  // turn the ledPin on
-  digitalWrite(ledPin, HIGH);  
-  // stop the program for  milliseconds:
-  delay(sensorValue);          
-  // turn the ledPin off:        
-  digitalWrite(ledPin, LOW);   
-  // stop the program for for  milliseconds:
-  delay(sensorValue);                  
+  int threshold = 512;
+  if(analogRead(potPin) > threshold){ digitalWrite(ledPin, HIGH);}
+  else{ digitalWrite(ledPin, LOW);}
 }
 
